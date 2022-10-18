272963813f2c8e2e39914f6e6dd88c8636442710
Deepanshu Gupta <89475845+Deepanshu1008@users.noreply.github.com>
2022-10-19 01:13:22 +0530

    Added quiz app java files

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is the capital of India?",
+            "Which one is not the programming language?",
+            "Which app is not owned by meta company?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Dollar","Yen","Euro","Rupee"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Rupee",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+
+}
